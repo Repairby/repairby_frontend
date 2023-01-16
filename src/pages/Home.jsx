5d90d9af8d03e0a9c365082781e30866729462e0
yejin <vgg789@naver.com>
2023-01-16 21:32:18 +0900

    [Feat] make homepage menubar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { device } from "../Device";
+import { IoIosMenu } from "react-icons/io";
 
 const Home = () => {
+  const [menu, setmenu] = useState(false);
+
   return (
     <HomePageWrapper>
       <HomePageHeader>
         <img src="/images/Nav/Logo.png" alt="Logo" />
-        <MenuBox>
+        <MenuBox menu={menu}>
           <Menu>회사소개</Menu>
           <Menu>고객센터</Menu>
           <Menu>내 견적 조회</Menu>
           <MainMenu>무료 견적 받기</MainMenu>
         </MenuBox>
+        <Menubar
+          onClick={() => {
+            setmenu(!menu);
+          }}
+        >
+          <IoIosMenu className="IoMenu" />
+        </Menubar>
       </HomePageHeader>
       <HomepageBody></HomepageBody>
     </HomePageWrapper>
@@ -29,16 +39,22 @@ const HomePageHeader = styled.div`
   justify-content: space-between;
   width: 1000px;
   height: 60px;
+  position: relative;
   margin: 12px auto;
-
-  @media all and (max-width: 1100px) {
-    width: 90%;
-  }
 
   img {
     width: 155px;
     height: 60px;
     margin-left: 2%;
+  }
+
+  @media all and (max-width: 1100px) {
+    width: 90%;
+  }
+
+  @media ${device.mobile} {
+    display: none;
+    display: block;
   }
 `;
 
@@ -46,12 +62,34 @@ const MenuBox = styled.div`
   display: flex;
   align-items: center;
   margin-right: 2%;
+
+  @media ${device.mobile} {
+    margin-top: 10px;
+    flex-direction: column;
+    align-items: flex-center;
+    display: ${({ menu }) => {
+      return menu === false ? "none" : "flex";
+    }};
+  }
 `;
 
 const Menu = styled.a`
   font-size: 16px;
   margin-right: 30px;
   cursor: pointer;
+
+  @media ${device.mobile} {
+    width: 100%;
+    font-size: 16px;
+    padding: 15px 0;
+    margin-right: 0;
+    cursor: pointer;
+    text-align: center;
+
+    :hover {
+      background-color: #bcbcbc;
+    }
+  }
 `;
 
 const MainMenu = styled.a`
@@ -60,6 +98,37 @@ const MainMenu = styled.a`
   padding: 13px;
   border-radius: 40px;
   cursor: pointer;
+
+  @media ${device.mobile} {
+    width: 100%;
+    text-align: center;
+    border: none;
+    border-radius: 0px;
+    padding: 15px 0;
+    font-size: 16px;
+    cursor: pointer;
+
+    :hover {
+      background-color: #bcbcbc;
+    }
+  }
+`;
+
+const Menubar = styled.a`
+  display: flex;
+  align-items: center;
+  position: absolute;
+  right: 32px;
+  top: 15px;
+  cursor: pointer;
+
+  .IoMenu {
+    font-size: 30px;
+  }
+
+  @media all and (min-width: 768px) {
+    display: none;
+  }
 `;
 
 // 홈페이지 바디 부분

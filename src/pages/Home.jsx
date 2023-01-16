190aed6c73a1594cb163cb709e255c901ea562aa
yejin <vgg789@naver.com>
2023-01-16 12:03:53 +0900

    [feat] media-query 적용

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,35 @@
 import React from "react";
+import styled from "styled-components";
+import { device } from "../Device";
 
 const Home = () => {
-  return <div>hii</div>; // 시범
+  return (
+    <HomePageWrapper>
+      <HomePageHeader>
+        {/* <img src="/images/Nav/Logo.png" alt="Logo" /> */}
+        hii
+      </HomePageHeader>
+    </HomePageWrapper>
+  );
 };
 
 export default Home;
+
+const HomePageWrapper = styled.div`
+  // 미디어 쿼리 예시
+  background-color: black;
+
+  @media ${device.mobile} {
+    background-color: red;
+  }
+
+  @media ${device.tablet} {
+    background-color: yellow;
+  }
+
+  @media ${device.pc} {
+    background-color: blue;
+  }
+`;
+
+const HomePageHeader = styled.div``;

e6991b730b1d9aaefd5ce2362449b6e94b752070
yejin <vgg789@naver.com>
2023-01-18 01:37:27 +0900

    [Feat] second PhoneRepair page layout

--- src/pages/PhoneRepair.jsx
+++ src/pages/PhoneRepair.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 import EachHeader from "../components/EachHeader";
+import MainPhoneRepairChoice from "./EachRepairChoice/MainPhoneRepairChoice";
+import PhoneRepairChoicePage2 from "./EachRepairChoice/PhoneRepairChoicePage2";
 
 const RepairList = [
   "액정 및 터치 문제",
@@ -18,55 +20,25 @@ const RepairList = [
 ];
 
 const PhoneRepair = () => {
-  const [ectClick, setEctClick] = useState(false);
-  const [questClick, setQuestClick] = useState(-1);
+  const [processCount, setProcessCount] = useState(1);
+  const [clickNumber, setClickNumber] = useState(-1);
 
   return (
     <MobileWrapper>
       <MobileContainer>
-        <EachHeader title="내 폰 수리하기" />
-        <MoblieScroll>
-          <PercentBar />
-          <Percent>50%</Percent>
-          <Question>
-            어떤 항목을 수리하길
-            <br /> 원하시나요?
-          </Question>
-          <QuestBoxWrapper>
-            {RepairList.map((quest, index) => (
-              <QuestBox
-                key={index}
-                id={index}
-                onClick={() => {
-                  setQuestClick(index);
-                  setEctClick(false);
-                }}
-                questClick={questClick}
-              >
-                {quest}
-              </QuestBox>
-            ))}
-            <EctBox
-              onClick={() => {
-                setEctClick(!ectClick);
-                setQuestClick(-1);
-              }}
-              ectClick={ectClick}
-            >
-              <EctSpan>기타</EctSpan>
-              <Described ectClick={ectClick}>
-                파손 정보를 직접 입력해주세요
-              </Described>
-              <EctInput
-                ectClick={ectClick}
-                onClick={e => {
-                  e.stopPropagation();
-                }}
-              />
-            </EctBox>
-          </QuestBoxWrapper>
-        </MoblieScroll>
-        <NextBox>다음</NextBox>
+        <EachHeader
+          title="내 폰 수리하기"
+          processCount={processCount}
+          setProcessCount={setProcessCount}
+        />
+        {processCount === 1 ? (
+          <MainPhoneRepairChoice
+            setProcessCount={setProcessCount}
+            setClickNumber={setClickNumber}
+          />
+        ) : processCount === 2 ? (
+          <PhoneRepairChoicePage2 clickNumber={clickNumber} />
+        ) : null}
       </MobileContainer>
     </MobileWrapper>
   );
@@ -89,122 +61,4 @@ const MobileContainer = styled.div`
   @media all and (max-width: 600px) {
     width: 100%;
   }
-`;
-
-const MoblieScroll = styled.div`
-  height: 90%;
-  overflow-x: hidden;
-  overflow-y: auto;
-`;
-
-const PercentBar = styled.div`
-  width: 50%;
-  height: 2px;
-  background-color: black;
-`;
-
-const Percent = styled.div`
-  padding-left: 48%;
-  margin-top: 10px;
-  font-size: 12px;
-`;
-
-const Question = styled.div`
-  text-align: start;
-  font-size: 25px;
-  font-weight: bold;
-  margin-top: 14px;
-  margin-left: 40px;
-  margin-bottom: 15px;
-  line-height: 38px;
-`;
-
-const QuestBoxWrapper = styled.div`
-  margin: 0 auto;
-  width: 90%;
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-  flex-flow: row wrap;
-`;
-
-const QuestBox = styled.div`
-  border: ${({ id, questClick }) => {
-    return id === questClick ? "1.5px solid black" : "1px solid #b2b2b2";
-  }};
-  border-radius: 10px;
-  white-space: pre-line;
-  width: 47%;
-  height: 55px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin-bottom: 20px;
-  cursor: pointer;
-  text-align: center;
-  line-height: 20px;
-  font-size: 14px;
-  font-weight: ${({ id, questClick }) => {
-    return id === questClick ? "bold" : "normal";
-  }};
-`;
-
-const EctBox = styled.div`
-  border: ${({ ectClick }) => {
-    return ectClick === false ? "1px solid #b2b2b2" : "1.5px solid black";
-  }};
-  border-radius: 10px;
-  width: 95%;
-  height: ${({ ectClick }) => {
-    return ectClick === false ? "55px" : "150px";
-  }};
-  margin-bottom: 20px;
-  cursor: pointer;
-  text-align: center;
-  line-height: 20px;
-  margin-bottom: 100px;
-  font-size: 14px;
-  font-weight: ${({ ectClick }) => {
-    return ectClick === false ? "normal" : "bold";
-  }};
-`;
-
-const EctSpan = styled.span`
-  display: block;
-  margin: 20px 0;
-`;
-
-const Described = styled.div`
-  display: ${({ ectClick }) => {
-    return ectClick === false ? "none" : "flex";
-  }};
-  justify-content: center;
-  color: gray;
-  font-size: 13px;
-`;
-
-const EctInput = styled.input`
-  display: ${({ ectClick }) => {
-    return ectClick === false ? "none" : "block";
-  }};
-  margin: 0 auto;
-  margin-top: 22px;
-  width: 85%;
-  border: none;
-  border-bottom: 1px solid black;
-  padding-bottom: 8px;
-`;
-
-const NextBox = styled.div`
-  border: 1px solid black;
-  position: absolute;
-  bottom: 0;
-  width: 100%;
-  height: 60px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: 18px;
-  background-color: black;
-  color: white;
 `;

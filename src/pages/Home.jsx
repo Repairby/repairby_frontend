266a063184babb2be2f66b30bb009f0fc45d3475
yejin <vgg789@naver.com>
2023-02-19 16:02:33 +0900

    [Fix] home.jsx 링크 설정 변경

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,12 +15,8 @@ const Home = () => {
           <MenuBox menu={menu}>
             <Menu>회사소개</Menu>
             <Menu>고객센터</Menu>
-            <Link to="/MyEstimatePage/">
-              <Menu>내 견적 조회</Menu>
-            </Link>
-            <Link to="/e_request/">
-              <MainMenu>무료 견적 받기</MainMenu>
-            </Link>
+            <Menu to="/MyEstimatePage/">내 견적 조회</Menu>
+            <MainMenu to="/e_request/">무료 견적 받기</MainMenu>
           </MenuBox>
           <Menubar
             onClick={() => {
@@ -117,8 +113,10 @@ const MenuBox = styled.div`
   display: flex;
   align-items: center;
   margin-right: 2%;
+  background-color: white;
 
   @media ${device.mobile} {
+    background-color: white;
     margin-top: 10px;
     flex-direction: column;
     align-items: flex-center;
@@ -128,7 +126,7 @@ const MenuBox = styled.div`
   }
 `;
 
-const Menu = styled.div`
+const Menu = styled(Link)`
   font-size: 16px;
   margin-right: 30px;
   cursor: pointer;
@@ -148,7 +146,7 @@ const Menu = styled.div`
   }
 `;
 
-const MainMenu = styled.div`
+const MainMenu = styled(Link)`
   font-size: 16px;
   border: 1px solid black;
   padding: 13px;
@@ -171,7 +169,7 @@ const MainMenu = styled.div`
   }
 `;
 
-const Menubar = styled.a`
+const Menubar = styled.div`
   display: flex;
   align-items: center;
   position: absolute;
